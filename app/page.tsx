74e8895c1f1bedfab1c171643337873b7354c291
Raghav <howtodonethis@gmail.com>
2026-08-06 11:17:42 +0530

    updated code
    updated code

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-slate-100">
-      <h1 className="text-5xl font-bold">
-        🚀 Next User Dashboard
-      </h1>
-    </main>
-  );
-}
